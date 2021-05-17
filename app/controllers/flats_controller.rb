a92b744746c218c2857b7d1569172c8cb84a66ca
Douglas Berkley <douglasmberkley@gmail.com>
2021-05-17 18:14:58 +0900

    Added before_action

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,18 +1,22 @@
 require "open-uri"
 
 class FlatsController < ApplicationController
+  before_action :set_flats, only: [:index, :show]
   def index
     # ActiveRecord (tomorrow)
     # @flats = Flat.all
-    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
-    @flats = JSON.parse(open(url).read)
   end
 
   def show
     # ActiveRecord (tomorrow)
     # @flat = Flat.find(params[:id])
+    @flat = @flats.find { |flat| flat['id'] == params[:id].to_i }
+  end
+
+  private
+
+  def set_flats
     url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
     @flats = JSON.parse(open(url).read)
-    @flat = @flats.find { |flat| flat['id'] == params[:id].to_i }
   end
 end
